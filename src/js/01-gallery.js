496a403b0bf2fcc8f7903c45f77dfad9925e5f2e
Dmitriy Gonharov <my.webpage057@gmail.com>
2025-03-21 00:43:16 +0200

    update video js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,49 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+console.log(galleryMarkup);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryMarkup(items) {
+    const markup = items.map(({preview,original,description}) => {
+        return `
+         <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>
+         `
+     }).join("");
+     return markup;
+};
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    
+    const isGalleryImageEl = evt.target.classList.contains("gallery__image");
+
+    if(!isGalleryImageEl) {
+    return;
+    }
+    console.log(evt.target);
+
+    openModal();
+};
+
+function openModal() {
+    let gallery = new SimpleLightbox('.gallery a', { 
+        overlayOpacity: 0.7,
+        captionsData: "alt",
+        captionDelay: 250,
+        navText: ['←','→'],
+    });
+    console.log(gallery);
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const localStorageKey = 'videoplayer-current-time';
+
+// Функция для сохранения времени в localStoragе
+
+function saveTime(event) {
+    console.log(event.seconds);
+    localStorage.setItem(localStorageKey, event.seconds);
+} 
+// функция для обновления времени
+player.on("timeupdate", saveTime);
+
+const getTime = localStorage.getItem(localStorageKey);
+player.setCurrentTime(getTime);

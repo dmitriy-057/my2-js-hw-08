3e257cbdb49b1e90457b5b4f7eb26fa60cdc2acd
Dmitriy Gonharov <my.webpage057@gmail.com>
2025-03-21 23:45:42 +0200

    update feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+const formEl = document.querySelector(".feedback-form");
 
+formEl.addEventListener("input", throttle(onHandleInputForm, 500));
+
+function onHandleInputForm(event) {
+    formData[event.target.name] = event.target.value;
+    console.log(formData);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};

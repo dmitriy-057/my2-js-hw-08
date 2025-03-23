71a8abfcdba2052e135459ebcab3028658a3740a
Dmitriy Gonharov <my.webpage057@gmail.com>
2025-03-23 02:17:09 +0200

    update feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,32 @@
 import throttle from 'lodash.throttle';
 const STORAGE_KEY = "feedback-form-state";
 const formData = {};
-const formEl = document.querySelector(".feedback-form");
+const formEl = document.querySelector(".feedback-form"); 
 
 formEl.addEventListener("input", throttle(onHandleInputForm, 500));
+formEl.addEventListener("submit", onFormSubmit);
 
+// task 1
 function onHandleInputForm(event) {
     formData[event.target.name] = event.target.value;
-    console.log(formData);
+    
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+    populateData();
 };
+// Получаем значение из хранилища. Если там что-то было обновляем DOM.
+function populateData() {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    if(savedData) {
+     console.log('savedData', savedData);
+    }
+    const parsedData = JSON.parse(savedData);
+// нужно записать данные в обьект при перезагрузе стр
+} 
+// task 3
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log('Submit form');
+    evt.currentTarget.reset();
+
+}

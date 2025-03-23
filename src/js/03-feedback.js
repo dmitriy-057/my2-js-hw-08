48cedddf4fb2e937e173608e32300190eca8c146
Dmitriy Gonharov <my.webpage057@gmail.com>
2025-03-23 11:39:27 +0200

    update feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,28 +5,35 @@ const formEl = document.querySelector(".feedback-form");
 
 formEl.addEventListener("input", throttle(onHandleInputForm, 500));
 formEl.addEventListener("submit", onFormSubmit);
-
+// Вызов ф-ции для заполнения инпутов если данные записаны в локальное хр-ще
+populateData();
 // task 1
 function onHandleInputForm(event) {
+    //записывает в обьект данные с инпутов
     formData[event.target.name] = event.target.value;
     
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-
-    populateData();
 };
 // Получаем значение из хранилища. Если там что-то было обновляем DOM.
 function populateData() {
     const savedData = localStorage.getItem(STORAGE_KEY);
     if(savedData) {
-     console.log('savedData', savedData);
+     const parsedData = JSON.parse(savedData);
+     console.log('parsedData',parsedData);
+
+     if(parsedData.email) {
+       formEl.elements["email"].value = parsedData.email;
+     }
+     if(parsedData.message) {
+        formEl.elements["message"].value = parsedData.message;
+     }
     }
-    const parsedData = JSON.parse(savedData);
-// нужно записать данные в обьект при перезагрузе стр
+
 } 
 // task 3
 function onFormSubmit(evt) {
     evt.preventDefault();
     console.log('Submit form');
     evt.currentTarget.reset();
-
+    localStorage.removeItem(STORAGE_KEY);
 }
